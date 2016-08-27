0b2559429bd40ec08fe76d7b1aabb1db45500c57
Michał Piotr Stankiewicz <stinger.dat@gmail.com>
2016-08-27 20:49:49 +0200

    feat(blog): ugly delete post view

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from .views import PostCreate, PostList
+from .views import PostCreate, PostList, PostDelete
 
 urlpatterns = [
     # posts
     url(r'^new$', PostCreate.as_view(), name='post_new'),
     url(r'^$', PostList.as_view(), name='post_list'),
+    url(r'^delete/(?P<pk>\d+)$', PostDelete.as_view(), name='post_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, DeleteView
 from django.views.generic import ListView
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.decorators import login_required
@@ -10,7 +10,7 @@ from .models import Post
 @method_decorator(login_required, 'dispatch')
 class PostCreate(CreateView):
     model = Post
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('blog:post_list')
     fields = ['title', 'content']
 
     def form_valid(self, form):
@@ -18,11 +18,15 @@ class PostCreate(CreateView):
         obj.created_by = self.request.user
         return super().form_valid(form)
 
-    def get_success_url(self):
-        return reverse_lazy('blog:post_list')
-
 
 class PostList(ListView):
     model = Post
     context_object_name = 'posts'
     paginate_by = 10
+
+
+# TODO(mps): ACCEPTS ANYONE TO DELETE!!!!
+class PostDelete(DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog:post_list')
+    context_object_name = 'post'
