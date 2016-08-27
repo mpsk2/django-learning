8f5b51313b70317cb76d5a16ed7db689117f66ab
Michał Piotr Stankiewicz <stinger.dat@gmail.com>
2016-08-27 11:06:38 +0200

    feat(project): basic project structure

---
+++ README.rst
@@ -0,0 +1,5 @@
+# Django learning
+
+Set of applications in Django to learn Django
+
+## 1. Set up structure
--- django_learning/urls.py
+++ django_learning/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
+    # base view with no processing
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+
     url(r'^admin/', admin.site.urls),
 ]
