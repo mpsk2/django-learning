ef86961e149e39cb4b7368dd01ec387ab126341c
Michał Piotr Stankiewicz <stinger.dat@gmail.com>
2016-08-27 11:37:20 +0200

    feat(blog): simple model with migration

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Publishable(models.Model):
+    creator = models.ForeignKey(User)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    last_modification_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Post(Publishable):
+    title = models.CharField(max_length=128, blank=False, null=False)
+    content = models.TextField()
+
+
+class Comment(Publishable):
+    content = models.TextField()
